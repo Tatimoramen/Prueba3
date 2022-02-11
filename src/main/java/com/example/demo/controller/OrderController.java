df6dfc9547d320636f8e5ae2f7fa5c2d764984c7
Luis-RodriguezB <lrodriguezbaltodano97@gmail.com>
2022-02-10 22:28:35 -0600

    ..

--- src/main/java/com/example/demo/controller/OrderController.java
+++ src/main/java/com/example/demo/controller/OrderController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/order")
 public class OrderController {
+
     private final OrderService orderService;
 
     public OrderController(OrderService orderService) { this.orderService = orderService; }
@@ -19,11 +20,13 @@ public class OrderController {
         List<Order> orderList = orderService.list();
         return ResponseEntity.ok().body(orderList);
     }
+
     @PostMapping
     public ResponseEntity<Order> create(@RequestBody Order order){
         Order orderSaved = orderService.save(order);
         return ResponseEntity.ok().body(orderSaved);
     }
+
     @PutMapping("{id}")
     public ResponseEntity<Order> put(@PathVariable("id") Long id, @RequestBody Order order){
         Order orderUpdate = orderService.put(order,id);
--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -9,7 +9,8 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "/product")
 public class ProductController {
-   private final ProductService productService;
+
+    private final ProductService productService;
 
     public ProductController(ProductService productService) {
         this.productService = productService;
